7f4d38ee4c50e3867e121614ac48ed9c87dcda1a
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2024-09-24 13:41:53 +0200

    add run script

---
+++ src/run.py
@@ -0,0 +1,64 @@
+import os
+
+from tz.osemosys import Model
+
+# ---------
+# CONFIGS
+# ---------
+
+output_path = '../outputs/solved_models/'
+
+countries_to_run = [
+    # 'GAB',
+    # 'GMB',
+    # 'HTI',
+    # 'MDG',
+    # 'PAK',
+    'PHL',
+    # 'RWA',
+]
+
+scenarios_to_run = [
+    'BAU',
+    'CPP',
+    'NDC',
+]
+
+
+
+print('-'*50)
+print('STARTING MODEL RUN')
+print('')
+
+for country in countries_to_run:
+    for scenario in scenarios_to_run:
+        print(f'Running: {country}-{scenario}')
+        # define path
+        path_to_model = f'../models/{country}/{country}-{scenario}/'
+        # load model from folder
+        model = Model.from_yaml(path_to_model)
+        # solve
+        model.solve(solver='gurobi')
+        # save
+        fname = model.id + '.nc'
+
+        dir_path = os.path.join(
+            output_path, 
+            country, 
+            country + '-' + scenario
+        )
+
+        # check if path exists
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        model.solution.to_netcdf(
+            os.path.join(
+                dir_path,
+                fname,
+            )
+        )
+
+        print('saved solution to: ' + dir_path + '/' + fname)
+
+print('-'*50)
