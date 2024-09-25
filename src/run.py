56ae832a8c8bb1880cada7b887a2a1632e7ee758
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2024-09-25 13:39:23 +0200

    setup model run and post-processing workflow

--- src/run.py
+++ src/run.py
@@ -1,12 +1,14 @@
 import os
+import matplotlib.pyplot as plt
 
+import osemoviz as viz
 from tz.osemosys import Model
 
 # ---------
 # CONFIGS
 # ---------
 
-output_path = '../outputs/solved_models/'
+output_path = '../outputs/results/'
 
 countries_to_run = [
     # 'GAB',
@@ -24,7 +26,9 @@ scenarios_to_run = [
     'NDC',
 ]
 
-
+# ---------
+# RUN MODELS
+# ---------
 
 print('-'*50)
 print('STARTING MODEL RUN')
@@ -39,8 +43,11 @@ for country in countries_to_run:
         model = Model.from_yaml(path_to_model)
         # solve
         model.solve(solver='gurobi')
-        # save
-        fname = model.id + '.nc'
+        
+        # ---------
+        # SAVE RESULTS
+        # ---------
+        fname = 'model-solution' + model.id + '.nc'
 
         dir_path = os.path.join(
             output_path, 
@@ -60,5 +67,144 @@ for country in countries_to_run:
         )
 
         print('saved solution to: ' + dir_path + '/' + fname)
+
+        # ---------
+        # POST PROCESSING
+        #
+        #   - There are 9 essential outputs we produce here as requested by the V20 group.
+        #   - We also produce some extra outputs that not required by the grant but useful for context.
+        #   - We produce all outputs using the tza-osemoviz package.
+        #
+        # ---------
+
+        # ---
+        # (1) Energy demand by sector and energy source (energy balance/year)
+
+        # make csv
+        (
+            viz
+            .get
+            .AnnualDemandByRegion(model)
+            .to_csv(
+                os.path.join(
+                    dir_path,
+                    'AnnualDemandByRegion.csv',
+                )
+            )
+        )
+
+        # export figure
+        ax = viz.plot.DemandByRegion(model)
+        plt.savefig(
+            os.path.join(
+                dir_path,
+                'AnnualDemandByRegion.pdf'
+            )
+        )
+
+        # ---
+        # (2) Power generation capacity by technology (MW in each year)
+
+        # make csv
+        (
+            viz
+            .get
+            .AnnualCapacityByTechnology(model)
+            .to_csv(
+                os.path.join(
+                    dir_path,
+                    'AnnualCapacityByTechnology.csv',
+                )
+            )
+        )
+
+        # export figure
+        ax = viz.plot.CapacityByTechnology(model)
+        plt.savefig(
+            os.path.join(
+                dir_path,
+                'AnnualCapacityByTechnology.pdf'
+            )
+        )
+
+        # ---
+        # (3) Power generation by technology (MWh/year)
+
+        # make csv
+        (
+            viz
+            .get
+            .AnnualGenerationByTechnology(model)
+            .to_csv(
+                os.path.join(
+                    dir_path,
+                    'AnnualGenerationByTechnology.csv',
+                )
+            )
+        )
+
+        # export figure
+        ax = viz.plot.GenerationByTechnology(model)
+        plt.savefig(
+            os.path.join(
+                dir_path,
+                'AnnualGenerationByTechnology.pdf'
+            )
+        )
+
+        # ---
+        # (4) Cost of technology (capital and O&M, per MW in each year)
+        # TODO
+        
+        # ---
+        # (5) Total investment required (timeframe) per year for new capacity plus grid and storage
+        # TODO
+
+        # ---
+        # (6) LCOE by technology (incl. timeframe)
+        # TODO
+
+        # ---
+        # (7) Average electricity price at the country level
+        # TODO
+
+        # ---
+        # (8) Energy access (access to electricity, % of population)
+        # TODO
+
+        # ---
+        # (9) Number of new jobs in power generation and grid
+        # TODO
+
+        # ---------
+        # ADDITIONAL OUTPUTS
+        # ---------
+
+        # ---
+        # (1) Emissions by technology
+
+        # export to csv
+        (
+            viz
+            .get
+            .AnnualEmissionsByTechnology(model)
+            .to_csv(
+                os.path.join(
+                    dir_path,
+                    'AnnualEmissionsByTechnology.csv',
+                )
+            )
+        )
+
+        # export figure figure
+        ax = viz.plot.EmissionsByTechnology(model)
+        plt.savefig(
+            os.path.join(
+                dir_path,
+                'AnnualEmissionsByTechnology.pdf'
+            )
+        )
+
+        
 
 print('-'*50)
