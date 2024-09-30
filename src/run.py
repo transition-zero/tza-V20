00f48b25b6801e046f98534d985aa54a9da8d6ef
djwels <dwelsby@LAPTOP-6PADDBSN>
2024-09-30 13:33:15 +0100

    Fixed indentation

--- src/run.py
+++ src/run.py
@@ -207,20 +207,22 @@ for country in countries_to_run:
             # functions
             # all variable costs converted to $/MWh by multiplying by 3.6
             # all generation (production) converted from PJ to TWh by dividing by 3.6
-            tech_load_factor=ProductionAnnual.sel(FUEL="primary-electricity") / (GrossCapacity * CapacityToActivityUnit)            
-            shadow_price_MWh=(marginal_cost_demand * YearSplit).sum(dims="TIMESLICE") * 3.6
-            carbon_price=marginal_cost_of_emissions_annual(model)
-            LCOE_Capacity=0.1 # representative 100 MW plant
-            LCOE_Generation=(LCOE_Capacity * tech_load_factor.YEAR * OperationalLife)/3.6
-            LCOE_Capital_Cost=CapitalCost.YEAR * LCOE_Capacity
-            LCOE_Fixed_Cost=FixedCost.YEAR * LCOE_Capacity * OperationalLife
-            LCOE_Variable_Cost=(VariableCost.YEAR * 3.6)
-            LCOE_Fuel_Cost=(shadow_price.YEAR)/(1 / InputActivityRatio)
-            LCOE_Carbon_Price=carbon_price.YEAR * EmissionsActivityRatio * LCOE_Generation
-            LCOE_Costs_Discount=((LCOE_Capital_Cost + LCOE_Fixed_Cost + LCOE_Variable_Cost + LCOE_Carbon_Cost) * (1+DiscountRate) ** (-OperationalLife))
-            LCOE_Generation_Discount= ((LCOE_Generation) * (1 + DiscountRate) ** (-OperationalLife))
-            LCOE_Output=(LCOE_Costs_Discount) / (LCOE_Generation_Discount)
-           
+        
+
+        tech_load_factor=ProductionAnnual.sel(FUEL='primary-electricity') / (GrossCapacity * CapacityToActivityUnit),            
+        shadow_price_MWh=(marginal_cost_demand * YearSplit).sum(dims="TIMESLICE") * 3.6,
+        carbon_price=marginal_cost_of_emissions_annual(model),
+        LCOE_Capacity=0.1 # representative 100 MW plant
+        LCOE_Generation=(LCOE_Capacity * tech_load_factor.YEAR * OperationalLife * 8.76),
+        LCOE_Capital_Cost=CapitalCost.YEAR * LCOE_Capacity,
+        LCOE_Fixed_Cost=FixedCost.YEAR * LCOE_Capacity * OperationalLife,
+        LCOE_Variable_Cost=(VariableCost.YEAR * 3.6),
+        LCOE_Fuel_Cost=(shadow_price.YEAR)/(1 / InputActivityRatio),
+        LCOE_Carbon_Price=carbon_price.YEAR * EmissionsActivityRatio * LCOE_Generation,
+        LCOE_Costs_Discount=((LCOE_Capital_Cost + LCOE_Fixed_Cost + LCOE_Variable_Cost + LCOE_Carbon_Cost) * (1+DiscountRate) ** (-OperationalLife)),
+        LCOE_Generation_Discount= ((LCOE_Generation) * (1 + DiscountRate) ** (-OperationalLife)),
+        LCOE_Output=(LCOE_Costs_Discount) / (LCOE_Generation_Discount),
+        
         # TODO
 
         # ---
